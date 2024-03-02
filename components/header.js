b99fb039dedb34d30919eda93315097e1618f459
Sumit Kumar <kumar.sumit0619@gmail.com>
2024-03-03 00:31:52 +0530

    4th commit

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 export default function Header(){
     return (<>
                 <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-                <h1>Welcome to this NextJS!</h1>
+                <h1>Welcome to this NextJS with sumit!</h1>
             </>);
 }
